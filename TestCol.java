95c11696a96ade2fda8a74c1d3b7ee10c1f78a15
Timothy Lawrence <timot.mamot@gmail.com>
2017-11-17 08:01:36 +0700

    Belom oop tapi

--- TestCol.java
+++ TestCol.java
@@ -1,5 +1,13 @@
+package jme3test.helloworld;
 
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.animation.AnimEventListener;
+import com.jme3.animation.Animation;
+import com.jme3.animation.LoopMode;
+import com.jme3.animation.SpatialTrack;
 import com.jme3.app.SimpleApplication;
+import com.jme3.bounding.BoundingVolume;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.collision.PhysicsCollisionEvent;
 import com.jme3.bullet.collision.PhysicsCollisionListener;
@@ -13,39 +21,61 @@ import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
+import com.jme3.terrain.geomipmap.TerrainGrid;
+import com.jme3.terrain.geomipmap.TerrainGridLodControl;
+import com.jme3.terrain.geomipmap.TerrainLodControl;
+import com.jme3.terrain.geomipmap.grid.ImageTileLoader;
+import com.jme3.terrain.geomipmap.lodcalc.DistanceLodCalculator;
+import com.jme3.terrain.heightmap.Namer;
+import com.jme3.texture.Texture;
+import java.util.HashMap;
 import java.util.Random;
 //
+
 /**
  *
  * @author Teuku Hashrul NPM : 2016730067
  */
-public class TestCol extends SimpleApplication implements ActionListener, AnalogListener {
+public class TestCol extends SimpleApplication implements ActionListener, AnalogListener, AnimEventListener {
 
-    private Geometry chara, obstacle, terrain;
+    private Geometry chara, obstacle, landscape;
+    
+    private TerrainGrid terrain;
+
+    private Node player;
+    
+    private Material mat_terrain;
 
     private int score; // nampung scorenyaaa
 
     private int highScore; //nampung highscore nya
 
-    private CharacterControl charaControl; 
+    private CharacterControl charaControl, character;
     private RigidBodyControl obstacleControl, terrainControl;
 
     private BitmapFont defaultFont; // font yang dipake
+
+    private AnimChannel channel;
+    private AnimControl control;
+    private AnimChannel animationChannel;
     
+     private float grassScale = 64;
     /*
-       attribut buat nanti wadah munculin textnya 
+      attribut buat nanti wadah munculin textnya 
     fpsScore : score sementara 
     highSCore : buat nampung high scorenya
     pressStart : buat panduan  klik start , sama reset game
-    */
-    private BitmapText fpsScoreText, pressStart, fpsHighScoreText; 
-    
+     */
+    private BitmapText fpsScoreText, pressStart, fpsHighScoreText;
 
     // boolean buat nge toggle gamenya start
     private boolean starting = false;
@@ -66,7 +96,7 @@ public class TestCol extends SimpleApplication implements ActionListener, Analog
 
         // Setting bullet app state
         bas = new BulletAppState();
-        bas.setDebugEnabled(true);
+        //bas.setDebugEnabled(true);
         stateManager.attach(bas);
 
         // dismissing fps counter and stat 
@@ -93,6 +123,48 @@ public class TestCol extends SimpleApplication implements ActionListener, Analog
         //intialize score and highScore to 0 
         highScore = 0;
         score = 0;
+        
+            Node model = new Node("model");
+            model.attachChild(landscape);
+
+            //animation parameters
+            float animTime = 5;
+            int fps = 25;
+            float totalXLength = 90;
+
+            //calculating frames
+            int totalFrames = (int) (fps * animTime);
+            float dT = animTime / totalFrames, t = 0;
+            float dX = totalXLength / totalFrames, x = 0;
+            float[] times = new float[totalFrames];
+            Vector3f[] translations = new Vector3f[totalFrames];
+            Quaternion[] rotations = new Quaternion[totalFrames];
+            Vector3f[] scales = new Vector3f[totalFrames];
+            for (int i = 0; i < totalFrames; ++i) {
+                times[i] = t;
+                t += dT;
+                translations[i] = new Vector3f(0, 0, -x);
+                x += dX;
+                rotations[i] = Quaternion.IDENTITY;
+                scales[i] = Vector3f.UNIT_XYZ;
+            }
+            SpatialTrack spatialTrack = new SpatialTrack(times, translations, rotations, scales);
+
+            //creating the animation
+            Animation spatialAnimation = new Animation("anim", animTime);
+            spatialAnimation.setTracks(new SpatialTrack[]{spatialTrack});
+
+            //create spatial animation control
+            AnimControl control = new AnimControl();
+            HashMap<String, Animation> animations = new HashMap<String, Animation>();
+            animations.put("anim", spatialAnimation);
+            control.setAnimations(animations);
+            model.addControl(control);
+
+            rootNode.attachChild(model);
+            //run animation
+            control.createChannel().setAnim("anim");
+
     }
 
     /*
@@ -131,39 +203,70 @@ public class TestCol extends SimpleApplication implements ActionListener, Analog
         11 add the character control into the physicsSpace
         12 return geomtetry 
      */
-    protected Geometry makeChara() {
-        Box b = new Box(1, 1, 1);
-        chara = new Geometry("Character", b);
-        chara.setLocalTranslation(0.0f, -2.8000004f, -40.61673f);
-        Material mat1 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat1.setColor("Color", ColorRGBA.Red);
-        chara.setMaterial(mat1);
-        BoxCollisionShape box = new BoxCollisionShape(chara.getWorldScale());
+    protected Node makeChara() {
+//        Box b = new Box(1, 1, 1);
+//        chara = new Geometry("Character", b);
+//        chara.setLocalTranslation(0.0f, -2.8000004f, -40.61673f);
+//        BoxCollisionShape box = new BoxCollisionShape(chara.getWorldScale());
 
-        charaControl = new CharacterControl(box, speed);
+        player = (Node) assetManager.loadModel("Models/TOLONG/TOLONG.j3o");
+        player.setLocalScale(0.35f);
+        player.setLocalTranslation(0, 0, -50f);
+        //    BoundingVolume bb = (BoundingVolume)player.getWorldBound();
+        CapsuleCollisionShape capsule = new CapsuleCollisionShape(1f, 2f);
 
-        chara.addControl(charaControl);
-        charaControl.setJumpSpeed(30);
-        charaControl.setFallSpeed(100);
-        charaControl.setGravity(80);
+        character = new CharacterControl(capsule, 0.35f);
+//        character.setPhysicsLocation(new Vector3f(0, 0, 0));
+//        charaControl = new CharacterControl(box, speed);
 
-        bas.getPhysicsSpace().add(charaControl);
+//        chara.addControl(charaControl);
+        player.addControl(character);
+//  charaControl.setJumpSpeed(30);
+//        charaControl.setFallSpeed(100);
+//        charaControl.setGravity(80);
 
-        return chara;
+        character.setJumpSpeed(12);
+        character.setFallSpeed(40);
+        character.setGravity(20);
+
+//        bas.getPhysicsSpace().add(charaControl);
+        bas.getPhysicsSpace().add(character);
+
+        // We add light so we see the scene
+        AmbientLight al = new AmbientLight();
+        al.setColor(ColorRGBA.White.mult(1.3f));
+        rootNode.addLight(al);
+
+        DirectionalLight dl = new DirectionalLight();
+        dl.setColor(ColorRGBA.White);
+        dl.setDirection(new Vector3f(2.8f, -2.8f, -2.8f).normalizeLocal());
+        rootNode.addLight(dl);
+
+//        control = player.getChild("Dragon 2").getControl(AnimControl.class);
+//        control.addListener(this);
+//        channel = control.createChannel();
+//        channel.setAnim("Run");
+//        animationChannel = control.createChannel();
+//        bas.getPhysicsSpace().add(character);
+
+        return player;
     }
 
     /**
-     * Method buat bikin obstaclenya 
-     * collision shapenya gue bikin tipe boxcollisionshape biar sesuai kotaknya parameternya kan vector3f ,jadi
-     * gue ambil v3nya si geometrynya biar ukuran collision shape nya sama geometry nya sesuai
-     * 
-     * pokonya konsep kasih physic itu 
-     * 1. bikin collision shape 
-     * 2. bikin rigid body control (karena terrain jadi pake rigid body bukan character control) trus parameternya 
-     * ( parameter 1 : collision shape nya yang dibikn di no 1 , parameter 2 : massa nya (massa 0 : statis contohnya floor/ terrain kalo yang character , obstacle massa nya jangan 0)
-     * 3. si geometry / node / spatial add control si rigid  bodynya
-     * 4. masukin si rigid bodynya ke  bullet app state(bas)
-     * @return 
+     * Method buat bikin obstaclenya collision shapenya gue bikin tipe
+     * boxcollisionshape biar sesuai kotaknya parameternya kan vector3f ,jadi
+     * gue ambil v3nya si geometrynya biar ukuran collision shape nya sama
+     * geometry nya sesuai
+     *
+     * pokonya konsep kasih physic itu 1. bikin collision shape 2. bikin rigid
+     * body control (karena terrain jadi pake rigid body bukan character
+     * control) trus parameternya ( parameter 1 : collision shape nya yang
+     * dibikn di no 1 , parameter 2 : massa nya (massa 0 : statis contohnya
+     * floor/ terrain kalo yang character , obstacle massa nya jangan 0) 3. si
+     * geometry / node / spatial add control si rigid bodynya 4. masukin si
+     * rigid bodynya ke bullet app state(bas)
+     *
+     * @return
      */
     protected Geometry makeObstacleDarat() {
         Box b = new Box(1, 1, 1);
@@ -184,7 +287,7 @@ public class TestCol extends SimpleApplication implements ActionListener, Analog
         kanan : -6.0915997f tengah : 2.0915997f kiri : 7.0915997f
      nanti pas di simple update pas box nya z nya udah -100 gue bakal setphysic location buat balikin si box
     nah di method ini , gue generate posisi x nya nanti masukin ke parameter x nya si set physic location
-    */
+     */
     public float obsDaratPositionGenerator() {
         Random rand = new Random();
         float kiri = 7.0915997f;
@@ -200,32 +303,60 @@ public class TestCol extends SimpleApplication implements ActionListener, Analog
         }
     }
 
+    public void initMaterial() {
+        // TERRAIN TEXTURE material
+        this.mat_terrain = new Material(this.assetManager, "Common/MatDefs/Terrain/HeightBasedTerrain.j3md");
+
+
+        // GRASS texture
+        Texture grass = this.assetManager.loadTexture("Textures/Terrain/splat/dirt.jpg");
+        grass.setWrap(Texture.WrapMode.Repeat);
+        this.mat_terrain.setTexture("region1ColorMap", grass);
+        this.mat_terrain.setVector3("region1", new Vector3f(-10, 0, this.grassScale));
+        
+        this.mat_terrain.setFloat("slopeTileFactor", 32);
+
+        this.mat_terrain.setFloat("terrainSize", 513);
+    }
+    
     /**
-     * Method buat bikin floornya 
-     * collision shapenya gue bikin tipe boxcollisionshape biar sesuai kotaknya parameternya kan vector3f ,jadi
-     * gue ambil v3nya si geometrynya biar ukuran collision shape nya sama geometry nya sesuai
-     * 
-     * pokonya konsep kasih physic itu 
-     * 1. bikin collision shape 
-     * 2. bikin rigid body control (karena terrain jadi pake rigid body bukan character control) trus parameternya 
-     * ( parameter 1 : collision shape nya yang dibikn di no 1 , parameter 2 : massa nya (massa 0 : statis contohnya floor/ terrain kalo yang character , obstacle massa nya jangan 0)
-     * 3. si geometry / node / spatial add control si rigid  bodynya
-     * 4. masukin si rigid bodynya ke  bullet app state(bas)
-     * @return 
+     * Method buat bikin floornya collision shapenya gue bikin tipe
+     * boxcollisionshape biar sesuai kotaknya parameternya kan vector3f ,jadi
+     * gue ambil v3nya si geometrynya biar ukuran collision shape nya sama
+     * geometry nya sesuai
+     *
+     * pokonya konsep kasih physic itu 1. bikin collision shape 2. bikin rigid
+     * body control (karena terrain jadi pake rigid body bukan character
+     * control) trus parameternya ( parameter 1 : collision shape nya yang
+     * dibikn di no 1 , parameter 2 : massa nya (massa 0 : statis contohnya
+     * floor/ terrain kalo yang character , obstacle massa nya jangan 0) 3. si
+     * geometry / node / spatial add control si rigid bodynya 4. masukin si
+     * rigid bodynya ke bullet app state(bas)
+     *
+     * @return
      */
     protected Geometry makeFloor() {
-        Box b = new Box(10, .2f, 150);
+        
+        Box b = new Box(10, .2f, 180);
         Vector3f v3 = new Vector3f(10, .2f, 200);
-        terrain = new Geometry("the Floor", b);
-        terrain.setLocalTranslation(0, -4, -5);
-        Material mat1 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat1.setColor("Color", ColorRGBA.Gray);
-        terrain.setMaterial(mat1);
+        landscape = new Geometry("the Floor", b);
+        landscape.setLocalTranslation(0, -4, -5);
+        landscape.setLocalScale(1f, 1.5f, 6f);
+        Material mat1 = new Material(assetManager, "Common/MatDefs/Terrain/HeightBasedTerrain.j3md");
+        landscape.setMaterial(mat1);
+        
+        Texture grass = this.assetManager.loadTexture("Textures/Terrain/splat/dirt.jpg");
+        grass.setWrap(Texture.WrapMode.Repeat);
+        mat1.setTexture("region1ColorMap", grass);
+        mat1.setVector3("region1", new Vector3f(-10, 0, this.grassScale));
+        mat1.setFloat("slopeTileFactor", 32);
+        mat1.setFloat("terrainSize", 513);
+        
         BoxCollisionShape box = new BoxCollisionShape(v3); // 1.
         terrainControl = new RigidBodyControl(box, 0.0f);  // 2.
-        terrain.addControl(terrainControl); // 3.
+        landscape.addControl(terrainControl); // 3.
         bas.getPhysicsSpace().add(terrainControl); //4
-        return terrain;
+        return landscape;
     }
 
     /*
@@ -251,7 +382,7 @@ public class TestCol extends SimpleApplication implements ActionListener, Analog
                 System.out.println(obj.x + "f ," + obj.y + "f ," + obj.z + "f");
             }
             if (name.equals("Jump") && !isPressed) {
-                charaControl.jump();
+                character.jump();
             }
         }
         if (name.equals("Start") && !isPressed) {
@@ -264,7 +395,7 @@ public class TestCol extends SimpleApplication implements ActionListener, Analog
             score = 0;
             obstacleControl.setPhysicsLocation(new Vector3f(0.0f, -2.8000004f, 40.61673f));
 
-            charaControl.setPhysicsLocation(new Vector3f(0.0f, -2.8000004f, -40.61673f));
+            character.setPhysicsLocation(new Vector3f(0, 0, -50f));
 
             loadText(fpsScoreText, "Current score : 0", defaultFont, 0, 3, 10);
             loadText(pressStart, "Press v to Start ", defaultFont, 0, 5, 10);
@@ -274,29 +405,31 @@ public class TestCol extends SimpleApplication implements ActionListener, Analog
 
     @Override
     /**
-     * 
+     *
      */
     public void simpleUpdate(float tpf) {
         /*
          nah if pertama tuh tadi gamenya ga bakal mulai sebelum player klik v buat start 
          kalo belom di toggle boolean nya ga bakal mulai simple updatenya
-        */
+         */
+
         if (starting) {
-            
+
             /*
              ini buat ngejalanin obstacle musuh ke arah karakter 
              INGAT ! kalo jalanin obstacle yang udah dikasih physics jangan pake setlocal translation 
              tapi pake setPhysics location supaya collision shape nya juga gerak 
-            */
-            Vector3f obsPos = obstacleControl.getPhysicsLocation(); 
-            obstacleControl.setPhysicsLocation(obsPos.set(obsPos.x, obsPos.y, obsPos.z - tpf * 30)); // 2
-            
-            
-            /**
-             *  ini buat ngecek kalo obstacle posisinya udah dibelakang kamera dibalikin ke depan lagi
-             * nah cuman buat ngerandom posisi lane nya gue pas ngeset x nya pake method obsDaratPositionGenerator
              */
+            Vector3f obsPos = obstacleControl.getPhysicsLocation();
+            obstacleControl.setPhysicsLocation(obsPos.set(obsPos.x, obsPos.y, obsPos.z - tpf * 30)); // 2
+            landscape.move(0, 0, -7 * tpf);
             
+
+            /**
+             * ini buat ngecek kalo obstacle posisinya udah dibelakang kamera
+             * dibalikin ke depan lagi nah cuman buat ngerandom posisi lane nya
+             * gue pas ngeset x nya pake method obsDaratPositionGenerator
+             */
             if (obsPos.z < -100) {  //Reset obstacle
                 obstacleControl.setPhysicsLocation(new Vector3f(obsDaratPositionGenerator(), -2.8000004f, 40.61673f));
             }
@@ -309,10 +442,10 @@ public class TestCol extends SimpleApplication implements ActionListener, Analog
              kalo karakter gue kedorong dikit aja terhadap znya maka gue pastiin kalah atau si y nya udah - 3 itu kalo jatoh kiri kanan
              
             nah kalo kalah si boolean starting tadi yang nge toggle semuanya gue bikin false lagi biar selesai game nya
-            */
-            Vector3f charaPos = charaControl.getPhysicsLocation();
+             */
+            Vector3f charaPos = character.getPhysicsLocation();
             //nabrak                   // jatoh
-            if (charaPos.z < -40.62f || charaPos.y < -3.0f) {
+            if (charaPos.z < -60.62f || charaPos.y < -3.0f) {
                 starting = !starting;
                 System.out.println("LOSE !");
                 pressStart.setText("You Lose , press G to Reset ! ");
@@ -325,7 +458,7 @@ public class TestCol extends SimpleApplication implements ActionListener, Analog
 
             score++; // nge counter scorenya 
             System.out.println("score : " + score);
-           
+
             // abis di counter di set ke wadah scorenya secara continously
             fpsScoreText.setText("Current score: " + score);
 
@@ -333,20 +466,19 @@ public class TestCol extends SimpleApplication implements ActionListener, Analog
 
     }
 
-    
     /*
      ini override analoglistener buat kiri kanan
-    */
+     */
     @Override
     public void onAnalog(String name, float value, float tpf) {
         if (starting) {
             if (name.equals("Left")) {
-                Vector3f obsPos = charaControl.getPhysicsLocation();
-                charaControl.setPhysicsLocation(obsPos.set(obsPos.x + tpf * 10, obsPos.y, obsPos.z));
+                Vector3f obsPos = character.getPhysicsLocation();
+                character.setPhysicsLocation(obsPos.set(obsPos.x + tpf * 10, obsPos.y, obsPos.z));
             }
             if (name.equals("Right")) {
-                Vector3f obsPos = charaControl.getPhysicsLocation();
-                charaControl.setPhysicsLocation(obsPos.set(obsPos.x - tpf * 10, obsPos.y, obsPos.z));
+                Vector3f obsPos = character.getPhysicsLocation();
+                character.setPhysicsLocation(obsPos.set(obsPos.x - tpf * 10, obsPos.y, obsPos.z));
             }
         }
     }
@@ -366,6 +498,19 @@ public class TestCol extends SimpleApplication implements ActionListener, Analog
         txt.setLocalTranslation(txt.getLineWidth() * x, txt.getLineHeight() * y, z);
         txt.setText(text);
         guiNode.attachChild(txt);
+    }
+
+    @Override
+    public void onAnimCycleDone(AnimControl control, AnimChannel channel, String animName) {
+
+        channel.setAnim("Run");
+        channel.setLoopMode(LoopMode.Loop);
+        channel.setSpeed(2.5f);
+
+    }
+
+    @Override
+    public void onAnimChange(AnimControl control, AnimChannel channel, String animName) {
     }
 
 }
